8e7719d8e21ce3a7f63c8972fe3e89adf0af1d23
luizamujdei <luiza.mujdei@gmail.com>
2024-12-12 15:17:50 +0200

    [Challenge-1] Implemented additional changes requested in PR1

--- letter-sender/src/main/java/com/christmas/letter/sender/model/ChristmasLetter.java
+++ letter-sender/src/main/java/com/christmas/letter/sender/model/ChristmasLetter.java
@@ -3,10 +3,15 @@ package com.christmas.letter.sender.model;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ChristmasLetter {
 
-    @Email
+    @Email(message = "Email should be valid")
     @NotNull(message = "Email is a mandatory field")
     private String email;
 
@@ -18,43 +23,4 @@ public class ChristmasLetter {
 
     @NotEmpty(message = "Location is a mandatory field")
     private String location;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getWishes() {
-        return wishes;
-    }
-
-    public void setWishes(String wishes) {
-        this.wishes = wishes;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public ChristmasLetter(String email, String name, String wishes, String location) {
-        this.email = email;
-        this.name = name;
-        this.wishes = wishes;
-        this.location = location;
-    }
 }
--- letter-sender/src/test/java/com/christmas/letter/sender/controller/ChristmasLetterControllerTest.java
+++ letter-sender/src/test/java/com/christmas/letter/sender/controller/ChristmasLetterControllerTest.java
@@ -3,12 +3,17 @@ package com.christmas.letter.sender.controller;
 import com.christmas.letter.sender.model.ChristmasLetter;
 import com.christmas.letter.sender.service.ChristmasLetterService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.stream.Stream;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -38,17 +43,23 @@ public class ChristmasLetterControllerTest {
                 .andExpect(status().isCreated());
     }
 
-    @Test
-    public void whenSendChristmasLetter_withInvalidRequest_ThenReturnBadRequest() throws Exception {
-        //Prepare message for the SNS Topic
-        String invalidTestMessage = objectMapper.writeValueAsString(new ChristmasLetter("testing", "Luiza", "", ""));
-
-        //Mock sending the message
+    @ParameterizedTest
+    @MethodSource("generateInvalidInput")
+    void whenSendChristmasLetter_withInvalidRequest_ThenReturnBadRequest(ChristmasLetter christmasLetter) throws Exception {
         mockMvc.perform(post(PATH)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(invalidTestMessage))
+                        .content(objectMapper.writeValueAsString(christmasLetter)))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message").value("The validation has failed"));
+    }
+
+    private static Stream<Arguments> generateInvalidInput(){
+        return Stream.of(
+                Arguments.of(new ChristmasLetter("invalidEmail", "Luiza", "fimo", "Iasi")),
+                Arguments.of(new ChristmasLetter("test@gmail.com", "", "a dog", "Iasi")),
+                Arguments.of(new ChristmasLetter("test@gmail.com", "Luiza", "", "Iasi")),
+                Arguments.of(new ChristmasLetter("test@gmail.com", "Luiza", "books", ""))
+        );
     }
 
 }
